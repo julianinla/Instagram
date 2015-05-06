cacf58ec8784cf755be6ee69e065af1e53284d39
Julian van Riet <vanrietjulian@gmail.com>
2015-05-05 19:42:57 -0700

    Milestone 3

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php 
 
-/*
-CLIENT INFO
-CLIENT ID	6ffbb1a0e9244168bdae9011caa8f3bc
-CLIENT SECRET	fbfc98c3740a4b7893dea4ae3862a027
-WEBSITE URL	http://localhost/Instagram/index.php
-REDIRECT URI	http://localhost/Instagram/index.php
-*/
-
 //Configuration for PHP server
 
 set_time_limit(0);
@@ -16,7 +8,23 @@ session_start();
 
 //Make constants using define
 
-define('client_id', '6ffbb1a0e9244168bdae9011caa8f3bc');
-define('client_secret', 'fbfc98c3740a4b7893dea4ae3862a027');
+define('client_ID', '6ffbb1a0e9244168bdae9011caa8f3bc');
+define('client_Secret', 'fbfc98c3740a4b7893dea4ae3862a027');
 define('redirectURI', 'http://localhost/Instagram/index.php');
 define('ImageDirectory', 'pics/');
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo client_ID ?>
+		&redirect_uri=<?php echo redirectURI ?>&response_type=code">LOGIN</a>
+		<!-- creating login for people to get approve for web app to use Instagram account
+		after getting aprroval now going to have info to play with it -->
+	</body>
+</html>

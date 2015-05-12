a8bd61512f91e9e4d92d96af10bf0957a3d20436
Julian van Riet <vanrietjulian@gmail.com>
2015-05-11 18:31:03 -0700

    Milestone 8 Code I Forgot

--- index.php
+++ index.php
@@ -17,12 +17,20 @@ define('ImageDirectory', 'pics/');
 function connectToInstagram($url) {
 	$ch = curl_init();
 
-	curl_setopt_array($ch, array[
+	curl_setopt_array($ch, array(
 		CURLOPT_URL => $url,
 		CURLOPT_RETURNTRANSFER => true,
 		CURLOPT_SSL_VERIFYPEER => false,
-		CURLOPT_SSL_VERIFYHOST => 2
-	]);
+		CURLOPT_SSL_VERIFYHOST => 2,
+	));
+	$result = curl_exec($ch);
+	curl_close($ch);
+	return $result;
+}
+
+//function to get userid cause username doesnt allow us to get pictures
+function getUserID($userName) {
+
 }
 
 if (isset($_GET['code'])) {
@@ -42,9 +50,11 @@ if (isset($_GET['code'])) {
 	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); //work, set to true
 
 
-$result = curl_exec($curl);
-curl_close();
+	$result = curl_exec($curl);
+	curl_close($curl);
 
+	$results = json_decode($result, true);
+	echo $results['user']['username'];
 }
 
 else {

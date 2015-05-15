d2b68bfb993681d365c52d9835153e04662e5141
Julian van Riet <vanrietjulian@gmail.com>
2015-05-15 16:30:46 -0700

    Media Queries

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html>
 	<head>
 		<title>Instagram</title>
+		<meta name="viewport" content="width-device width, initial-scale=1.0">
 	</head>
 		<link rel="stylesheet" href="css/main.css">
 		<link rel="stylesheet" href="css/bootstrap-theme.css">
